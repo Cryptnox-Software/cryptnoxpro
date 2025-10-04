c2bef318fcd43e8e895dffb425d555aa8c912bf4
YanNaingWinDev <yannw.dev@gmail.com>
2025-10-05 01:47:18 +0700

    Resolved flake8 errors

--- cryptnoxpro/lib/cryptos/mnemonic.py
+++ cryptnoxpro/lib/cryptos/mnemonic.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 Mnemonic phrase generation and seed handling (BIP39-style operations).
+
+This module is based on code from pybtctools:
+https://github.com/danvergara/pybtctools/blob/32b50d324d1c9432719ba08059886458fa6b42eb/bitcoin/mnemonic.py
 """
 import hmac
 import random
