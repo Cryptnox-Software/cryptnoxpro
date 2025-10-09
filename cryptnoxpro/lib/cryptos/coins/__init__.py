e0dbda35f3c037349a62720e55eb2a1de843773e
YanNaingWinDev <yannw.dev@gmail.com>
2025-10-09 18:13:52 +0700

    Resolved runtime import errors

--- cryptnoxpro/lib/cryptos/coins/__init__.py
+++ cryptnoxpro/lib/cryptos/coins/__init__.py
@@ -6,27 +6,4 @@ Re-exports available coin implementations for convenience.
 
 from .bitcoin import *
 
-try:
-    from .bitcoin_cash import *
-except Exception:
-    pass
-
-try:
-    from .bitcoin_gold import *
-except Exception:
-    pass
-
-try:
-    from .dash import *
-except Exception:
-    pass
-
-try:
-    from .dogecoin import *
-except Exception:
-    pass
-
-try:
-    from .litecoin import *
-except Exception:
-    pass
+__all__ = ['Bitcoin']
